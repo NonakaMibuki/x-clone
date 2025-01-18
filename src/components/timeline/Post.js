a611094c9552580559fc0a5b5b22c595be8637e8
{nonakamibuki} <mibukinonaka@gmail.com>
2025-01-18 15:00:00 +0900

    いいね機能追加

--- src/components/timeline/Post.js
+++ src/components/timeline/Post.js
@@ -1,23 +1,40 @@
-import React, { forwardRef } from "react";
+import React, { forwardRef, useState } from "react";
 import "./Post.css";
-import { Avatar } from "@mui/material";
-import { ChatBubbleOutline, FavoriteBorder, PublicOffOutlined, Repeat, VerifiedUser } from "@mui/icons-material";
+import { Avatar, IconButton, ThemeProvider, createTheme } from "@mui/material";
+import { ChatBubbleOutline, Favorite, FavoriteBorder, PublicOffOutlined, Repeat, VerifiedUser } from "@mui/icons-material";
 
+const Post = forwardRef(({ displayName, username, verified, text, image, avatar }, ref) => {
+  const [favorite, setFavorite] = useState(false);
 
-const Post = forwardRef(( {displayName, username, verified, text, image, avatar}, ref) => {
+  const handleClick = () => {
+    if (favorite === false) {
+      setFavorite(true);
+    } else {
+      setFavorite(false);
+    }
+  };
+
+  const theme = createTheme({
+    palette: {
+      secondary: {
+        main: "#f44336",
+      },
+    },
+  });
+
   return (
     <div className="post" ref={ref}>
       <div className="post__avatar">
-        <Avatar src={avatar}/>
+        <Avatar src={avatar} />
       </div>
       <div className="post__body">
         <div className="post__header">
           <div className="post__headerText">
-            <h3>{displayName}
-            <span className="post__headerSpecial">
-              <VerifiedUser className="post__badge" />
-              @{username}
-            </span>
+            <h3>
+              {displayName}
+              <span className="post__headerSpecial">
+                <VerifiedUser className="post__badge" />@{username}
+              </span>
             </h3>
           </div>
           <div className="post__headerDescription">
@@ -28,13 +45,14 @@ const Post = forwardRef(( {displayName, username, verified, text, image, avatar}
         <div className="post__footer">
           <ChatBubbleOutline fontSize="small" />
           <Repeat fontSize="small" />
-          <FavoriteBorder fontSize="small" />
+          <ThemeProvider theme={theme}>
+            <IconButton onClick={() => handleClick()}>{favorite ? <Favorite color="secondary" /> : <FavoriteBorder />}</IconButton>
+          </ThemeProvider>
           <PublicOffOutlined fontSize="small" />
         </div>
       </div>
     </div>
   );
-})
-
+});
 
 export default Post;

44c2df29b5e5ffc7bc675c66df81e350233c6f61
{nonakamibuki} <mibukinonaka@gmail.com>
2025-01-19 11:34:23 +0900

    初期ページをlogin画面に変更

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Timeline from "./components/timeline/Timeline";
 import Widget from "./components/widget/Widget";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 import Login from "./components/Login/Login";
 import Logout from "./components/Logout/Logout";
 
@@ -16,11 +16,15 @@ function App() {
         <Route
           path="/"
           element={
+            isAuth ? (
             <div className="app">
               <Sidebar isAuth={isAuth}/>
               <Timeline isAuth={isAuth}/>
               <Widget isAuth={isAuth}/>
             </div>
+            ):(
+              <Navigate to="/login" replace/>
+            )
           }
         />
         <Route path="/login" element={<Login setIsAuth={setIsAuth}/>}/>
